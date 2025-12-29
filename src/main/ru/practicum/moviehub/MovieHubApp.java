8538ce1a96d8eece5c859f135312c11fb051e097
Alexander Platonov <platon1560@gmail.com>
2025-12-29 16:03:12 +0300

    правки CodeStyle

--- src/main/ru/practicum/moviehub/MovieHubApp.java
+++ src/main/ru/practicum/moviehub/MovieHubApp.java
@@ -5,6 +5,7 @@ import ru.practicum.moviehub.store.MoviesStore;
 
 public class MovieHubApp {
     private static final int PORT = 8080;
+
     public static void main(String[] args) {
         final MoviesServer server = new MoviesServer(new MoviesStore(), PORT);
         Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
